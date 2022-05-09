d01a0577a4904cd65324d86e5485a24ad1db31ca
Frantisek Harnicar <frantisek.harnicar@student.fei.tuke.sk>
2022-05-09 22:43:23 +0200

    pridanie obrazkov--FINISH

--- app/Client/my-app/src/screens/TeacherTest.js
+++ app/Client/my-app/src/screens/TeacherTest.js
@@ -2,23 +2,29 @@ import React, { useState } from "react";
 import '../styles/tests.css'
 import testIcon from '../styles/img/testIcon.png'
 import close from '../styles/img/close.png'
-import { Button } from "react-bootstrap";
+import {Modal, Button} from "react-bootstrap"
 import Axios from "axios"
 import {useNavigate} from 'react-router-dom'
+
 
 
 function TeacherTest(props){
     const navigate = useNavigate()
     let publicate = new Date(props.datum_publikacie)
     const [isDelete, setIsDelete] = useState(false)
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     const deleteTest = () =>{
         Axios.post('http://localhost:3000/teacherdelete',({ id:props.id}))
         .then((res) => {
             if(res.data){
                setIsDelete(true)
+               setShow(false)
             }
         }) 
     }
+    
 
     
     return (
@@ -35,9 +41,30 @@ function TeacherTest(props){
                     <Button onClick={() => navigate('/addTest/'+(props.id))} className="greenButtons">Upraviť</Button>
                     <Button onClick={() => navigate('/teacherRating/'+(props.nazov_testu))} className="blueButtons" >Hodnotenie</Button>
                 </div>
-                    <img onClick={deleteTest} src={close} className="close"/>
+                    <img onClick={handleShow} src={close} className="close"/>
             </div>
         :<></>}
+        <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+                centered={true}
+                >
+                <Modal.Header closeButton className="informationModal">
+                    <Modal.Title>Spustiť test?</Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="informationModal">
+                    Po stlačení tlačidla <b>Odstraniť test</b> sa test nenavratne odstrani.
+                    Prajete si odstraniť test?
+                </Modal.Body>
+                <Modal.Footer className="informationModal">
+                    <Button className="change--question" variant="secondary" onClick={handleClose}>
+                    Zavrieť
+                    </Button>
+                    <Button className="delete--style" onClick={deleteTest} variant="primary">Odstraniť test</Button>
+                </Modal.Footer>
+                </Modal>
         </>
         
     );
